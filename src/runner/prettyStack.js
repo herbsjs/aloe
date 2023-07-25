d73ff5fb1dc434e770546bc6b7bf849425fce52d
David Lojudice Sobrinho <dalssoft@gmail.com>
2023-07-25 20:14:20 -0300

    fix(prettystack): fix prettyStack when error is not a Error instance

--- src/runner/prettyStack.js
+++ src/runner/prettyStack.js
@@ -12,6 +12,11 @@ const themes = {
 
 function prettyStack({ error, theme = themes.herbs }) {
 
+    if (!(error instanceof Error)) {
+        console.log(`\n       `, chalk.ansi256(theme['main'])('Error:'), chalk.ansi256(theme['message'])(chalk.bgAnsi256(theme['backgroundMessage'])(error)), `\n`)
+        return
+    }
+
     // Replace the error message with a colorized version
     let messageStack = error.stack.replace(error.message, chalk.ansi256(theme['message'])(chalk.bgAnsi256(theme['backgroundMessage'])(error.message)))
 
